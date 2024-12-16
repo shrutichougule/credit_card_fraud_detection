bfda3f9be5cbacb295c74332cf03b5c2667572fe
Shruti chougule <41620526+shrutichougule@users.noreply.github.com>
2024-12-16 14:11:46 +0530

    Delete download_load.sh

--- download_load.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl https://www.kaggle.com/code/rajputnavya/credit-card-fraud-detection/input?select=fraudTrain.csv | gsutil cp - gs://inputdatafiles/fraudTrain.csv
-curl https://www.kaggle.com/code/rajputnavya/credit-card-fraud-detection/input?select=fraudTest.csv | gsutil cp - gs://inputdatafiles/fraudTrain.csv
